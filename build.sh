5ed74d88d570caf9985c7600142b3aca406bc391
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-02-04 16:30:33 -0500

    Fixup entrypoint for Alpine-based images

--- build.sh
+++ build.sh
@@ -27,8 +27,8 @@ IMG_TAG=alpine-minimal-latest
 docker build -f Dockerfile.alpine-minimal -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
 docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
 
-DOCKER_REPO_BASE="latex-docker"
-IMG_TAG=alpine-minplus-latest
+#DOCKER_REPO_BASE="latex-docker"
+#IMG_TAG=alpine-minplus-latest
 
-docker build -f Dockerfile.alpine-minplus -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
-docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+#docker build -f Dockerfile.alpine-minplus -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
+#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
